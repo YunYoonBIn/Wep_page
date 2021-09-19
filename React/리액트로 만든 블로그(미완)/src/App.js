745201612753d64ecffaa1f76498604a7583afba
YunYoonBin <yubiny287@gmail.com>
2021-09-20 04:00:24 +0900

    웹페이지

--- React/리액트로 만든 블로그(미완)/src/App.js
+++ React/리액트로 만든 블로그(미완)/src/App.js
@@ -9,6 +9,8 @@ function App() {
 
   let [모달, 모달변경] = useState(false);
 
+  let [누른제목숫자, 누른제목숫자변경] = useState(0);
+
   function 제목바꾸기(params) {
     var newArray = [...글제목];
     newArray[2] = 'C언어';
@@ -32,21 +34,22 @@ function App() {
       <button onClick={제목바꾸기}>test제목 바꾸기</button>
 
       {
-        글제목.map(function (i) {
+        글제목.map(function (a,i) {
           return (
             <div className="list">
-              <h3> { i } <span onClick={() => { 좋아요변경(좋아요 + 1) }}>👍</span> {좋아요} </h3>
-              <p>9월 11일 작성</p>
+              <h3 onClick={ ()=>{ 누른제목숫자변경(i) } }> {a} 
+              <span onClick={ ( )=>{ 좋아요변경 (좋아요 + 1) } }>👍</span> { 좋아요 } </h3>
+              <p>9월 20일 적음</p>
               <hr />
             </div>
           )
         })
       }
 
-      <button onClick={() => { (모달변경(!모달)) }}>버튼</button>
+      <button onClick={() => { (모달변경(!모달)) }}>게시글 열고닫기</button>
       {
         모달 === true
-          ? <Modal> </Modal>
+          ? <Modal 글제목 = { 글제목 } 누른제목숫자 = { 누른제목숫자 }> </Modal>
           : null
       }
 
@@ -54,12 +57,12 @@ function App() {
   );
 }
 
-function Modal(params) {
+function Modal(props) {
   return (
     <div className="modal">
-      <h2>제목</h2>
-      <p>날짜</p>
-      <p>내용</p>
+      <h2>제목 : {props.글제목[props.누른제목숫자] }</h2>
+      <p>날짜 : 2021/9/20</p>
+      <p>내용 : 가나다라마바사아자차카타파하</p>
     </div>
   )
 }
